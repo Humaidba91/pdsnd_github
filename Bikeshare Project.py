941cf36de5b2bb93171cb4110c12ca63ec99ea64
Humaidba <humaidba.abumuhammad@gmail.com>
2019-10-16 20:05:14 +0700

    Additional change module line 59 and add new module in line 9

--- Bikeshare Project.py
+++ Bikeshare Project.py
@@ -6,6 +6,9 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 
+def method_new(input_city):
+    return input_city, input_month, input_day
+
 def get_filters():
     global input_month
     global input_day
@@ -35,7 +38,7 @@ def get_filters():
     while True:
         input_month = input("Please select a month or all: ").lower()
         if input_month not in month:
-            print ("You may have misspell the month, please try again!", end='')
+            print("You may have misspell the month, please try again!", end='')
             input_month = input("Please retype the month: ")
             
             continue
@@ -53,7 +56,7 @@ def get_filters():
    
 
     print('-'*40)
-    return input_city, input_month, input_day
+    return method_new(input_city)
 
 
 def load_data(city, month, day):
